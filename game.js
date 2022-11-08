d3c1197625cc0bbebad763275be680dbbe083a4d
darcy-murphy <113056025+darcy-murphy@users.noreply.github.com>
2022-11-08 14:23:55 -0500

    init

---
+++ game.js
@@ -0,0 +1,21 @@
+/* global Phaser */
+
+// Copyright (c) 2022 Darcy Murphy All rights reserved 
+//
+// Created by: Darcy Murphy
+// Createdon: Nov 2022
+// This is the Phaser3 game configuration file
+
+/**
+ * Start Phaser Game.
+ */
+const config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    // set backround color
+    backroundColor: 0x5f6e7a,
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
